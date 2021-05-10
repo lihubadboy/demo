6242234d571ffe5bf0e819d73d00997c5c83729a
lihubadboy <lih@esrichina.com.cn>
2021-05-10 09:00:09 +0800

    增加demo

---
+++ example/ArcGIS/renderer/renderer.js
@@ -0,0 +1,22 @@
+
+// 给精模
+const buildingRender = {
+    type: 'simple',
+    symbol: {
+      type: 'mesh-3d',
+      symbolLayers: [
+        {
+          type: 'fill',
+          material: {
+            color: [244, 232, 206],
+            colorMixMode: 'replace',
+          },
+          edges: {
+            type: 'solid',
+            color: [244, 232, 206],
+            size: 1,
+          },
+        },
+      ],
+    },
+  };
